04658c66bdc95a40318bedd85e6b4bde2b767c95
it21098314 <it21098314@my.sliit.lk>
2024-05-19 18:02:02 +0530

    again new commit

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ from app import add
 def test_add():
     assert add(1, 2) == 3
     assert add(-1, 1) == 0
-    assert add(-1, -3) == -5  # Corrected expected value to -4
+    assert add(-1, -3) == -4  # Corrected expected value to -4
 
 if __name__ == "__main__":
     pytest.main()
