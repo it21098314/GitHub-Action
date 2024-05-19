69ee2d6c3c48963cd0d7eac2b2f9ec03abfed591
it21098314 <it21098314@my.sliit.lk>
2024-05-19 17:54:28 +0530

    first commit

---
+++ test_app.py
@@ -0,0 +1,10 @@
+import pytest
+from app import add
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(-1, 1) == 0
+    assert add(-1, -3) == -4  # Corrected expected value to -4
+
+if __name__ == "__main__":
+    pytest.main()
